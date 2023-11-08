61601b70be02bd6d265f35953ba5b77ff2556d6e
forerunrun <50278392+forerunrun@users.noreply.github.com>
2023-11-08 23:55:26 +0000

    Add files via upload

---
+++ isMobile.js
@@ -0,0 +1,18 @@
+export const IsMobile = () => {
+    let isMobile = false
+    if ("maxTouchPoints" in navigator) {
+        isMobile = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+        isMobile = navigator.msMaxTouchPoints > 0;
+    } else {
+        var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+        if (mQ && mQ.media === "(pointer:coarse)") {
+            isMobile = !!mQ.matches;
+        } else if ('orientation' in window) {
+            isMobile = true; 
+        } else {
+            isMobile = ( /Android|Windows Phone|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) );
+        }
+    }
+    return isMobile;
+}
